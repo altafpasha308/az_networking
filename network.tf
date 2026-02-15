b0b507ebff4054768d2d628c211023f7240fdbc5
learner <learner@ow.com>
2026-02-15 08:13:51 +0000

    adding network plan

--- network.tf
+++ network.tf
@@ -1,13 +1,13 @@
 resource "azurerm_network_security_group" "example" {
   name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "Australia Southeast"
+  resource_group_name = "terraform-resources"
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "Australia Southeast"
+  resource_group_name = "terraform-resources"
   address_space       = ["10.0.0.0/16"]
   #dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
